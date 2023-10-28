049e76061f83041779cea1b17f8ea32d43b446b6
Dustin Hendriks <d.hendriks.tweakers@gmail.com>
2023-10-28 11:25:18 +0200

    Release initial version Clock PRO

---
+++ Source/Clock/Clock/FontPicker.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clock
+{
+    public partial class FontPicker : Window
+    {
+        public FontPicker()
+        {
+            InitializeComponent();
+
+            if (MainWindow.MainWindowInstance.StorageSettings.AppState == null) return;
+            this.FontFamilyComboBox.SelectedItem = MainWindow.MainWindowInstance.StorageSettings.AppState.FontFamily;
+            this.FontSizeSlider.Value = MainWindow.MainWindowInstance.StorageSettings.AppState.FontSize;
+        }
+
+        private void OnApplyClicked(object sender, RoutedEventArgs e)
+        {
+            if (FontFamilyComboBox.SelectedItem != null)
+            {
+                MainWindow.MainWindowInstance.SettingFontFamily((FontFamily)FontFamilyComboBox.SelectedItem);
+                MainWindow.MainWindowInstance.SettingFontSize(FontSizeSlider.Value);
+            }
+            MainWindow.MainWindowInstance.UpdateGeneral();
+        }
+    }
+
+}
